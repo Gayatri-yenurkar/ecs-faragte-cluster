e4f5a6745673a2ab4e7b4f25786394b6afa9198f
Gayatri-yenurkar <109518371+Gayatri-yenurkar@users.noreply.github.com>
2022-07-25 15:23:22 +0530

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,50 @@
+variable "aws-region" {
+  description = "AWS"
+  default     = "us-east-1"
+}
+variable "access-key" {
+  default ="AKIAV34IJCION3OR3C7R"
+}
+variable "secret-key" {
+  default = "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"
+}
+variable "ecs_task_execution_role_name" {
+  default = "ecs-task-execution-role"
+
+}
+variable "az_count" {
+  description = "No of AZ in a region"
+  default     = "2"
+}
+
+variable "app_image" {
+  description = "Docker image to run in ECS cluster"
+  default     = "nginx:latest"
+}
+variable "app_port" {
+  description = "port exposed by docker to direct traffic"
+  default     = "80"
+
+}
+variable "app_count" {
+  description = "Number of docker containers to run"
+  default     = 1
+}
+
+
+variable "fargate_cpu" {
+  description = "Fargate instance CPU unit"
+  default     = "1024"
+
+}
+variable "fargate_memory" {
+
+  description = "Fargate instance memory"
+  default     = "2048"
+
+}
+
+variable "aws_vpc_cidr" {
+  description = "The CIDR of the vpc"
+  default = "170.10.0.0/16"
+}
